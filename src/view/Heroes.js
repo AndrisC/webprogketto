c7da033b371e48dff434cead5b1c6175f54d5611
Andras Csipszer <csipszerandras@gmail.com>
2021-06-03 14:05:13 +0200

    [HTML,CSS] New hero-card & heroes page look... YEY!🥵

--- src/view/Heroes.js
+++ src/view/Heroes.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect, Component, useCallback} from 'react';
 import '../style/App.css';
+import '../style/Heroes.css';
+import '../style/Herocard.css';
 import {BrowserRouter as Link} from 'react-router-dom';
 import { wait } from '@testing-library/dom';
 
@@ -10,7 +12,7 @@ export default class Heroes extends React.Component {
         heroes: [],
         idCountFrom: 1,
     };
-    
+
 
     async componentDidMount() {
         const url = "https://www.superheroapi.com/api.php/3061607853876230/";
@@ -38,7 +40,7 @@ export default class Heroes extends React.Component {
         await this.setState({idCountFrom: this.state.idCountFrom+10});
         this.componentDidMount();
     }
-    
+
     async handlePrevButtonClick() {
         this.setState({loading: true});
         await this.setState({idCountFrom: this.state.idCountFrom-10});
@@ -46,7 +48,7 @@ export default class Heroes extends React.Component {
     }
 
     render() {
-        const idCounter = this.state.idCountFrom; 
+        const idCounter = this.state.idCountFrom;
         let button;
         if (idCounter <= 1) {
             button = <div><button onClick ={() => this.handleNextButtonClick()}>NEXT PAGE</button></div>;
@@ -57,32 +59,57 @@ export default class Heroes extends React.Component {
                        <button onClick ={() => this.handlePrevButtonClick()}>PREVIUOS PAGE</button> <button onClick ={() => this.handleNextButtonClick()}>NEXT PAGE</button>
                     </div>;
         }
+        if (true) {
+
+        }
 
         return (
-            <div>
-                {this.state.loading ?
-                    <div className="loading">loading...</div> :
-                    <div id="heroes">
-                        <ul className="flex-container">
-                        {this.state.heroes.map(hero => (
-                            <div key={hero.id.toString()} className="box">
-                                <li className="listed-text">
-                                    <img className="cover" src={hero.image.url}></img>
-                                    <h1>- {hero.name} -</h1>
-                                    <h2>{hero.biography["full-name"]}</h2>
-                                    <h3>{hero.work.occupation}</h3>
-                                    <h3>{hero.appearance.race}</h3>
-                                    <a href={'/hero/' + hero.id}>
-                                        <Link to={'/hero/' + hero.id}>More info</Link>
-                                    </a>
-                                </li>
-                            </div>
-                        ))     
-                        }
-                        </ul>
-                        <div>{button}</div>
-                    </div>                    
-                }
+            <div class="heroes-page">
+              {this.state.loading ?
+                <div class="loading">loading...</div> :
+
+                <div id="heroes">
+                  <div class="page-title">
+                    <h1>Hero collection</h1>
+                  </div>
+
+                  <div class="card-container">
+                    {this.state.heroes.map(hero => (
+                      <div key={hero.id.toString()} class="hero-card">
+                        <img class="hero-pic" src={hero.image.url}></img>
+                        <img class="hero-card-bg" src={hero.image.url}></img>
+
+                        <div class="hero-info">
+                          <span>Name:</span>
+                          <h2>{hero.name}</h2>
+
+                          <span>Full name:</span>
+                          {(hero.biography["full-name"] == "") ?
+                            <h2>Unknown</h2> :
+                            <h2>{hero.biography["full-name"]}</h2>
+                          }
+
+                          <span>Race:</span>
+                          {(hero.appearance.race == "null") ?
+                            <h2>Unknown</h2> :
+                            <h2>{hero.appearance.race}</h2>
+                          }
+
+                          <div class="btn-container">
+                            <a href={'/hero/' + hero.id}>
+                              <button class="more-info-btn" type="button">
+                                <Link to={'/hero/' + hero.id}>More info</Link>
+                              </button>
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+
+                  <div>{button}</div>
+                </div>
+              }
             </div>
         )
     }
